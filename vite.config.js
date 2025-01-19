ece79fea99729fc89ee596395efcc64d88fb9cb6
Charul00 <charulchim18@gmail.com>
2025-01-19 18:11:55 +0530

    Update Vite config to handle external CDN assets

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -9,6 +8,11 @@ export default defineConfig({
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') }
     ],
+  },
+  build: {
+    rollupOptions: {
+      external: ['@highlightjs/cdn-assets']
+    },
   },
   server: {
     headers: {
